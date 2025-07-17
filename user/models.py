3e143c5f193ebb6fe976cb9cbd6e964bd9b83614
Danyyl <dolynskydanyyl@gmail.com>
2025-07-17 04:20:02 +0300

    append all to cloudinary

--- user/models.py
+++ user/models.py
@@ -3,13 +3,11 @@ from django.conf import settings
 from django.templatetags.static import static
 from cloudinary.models import CloudinaryField
 
-# def user_directory_path(instance, filename):
-#     return f'users/avatars/user_{instance.user.id}/{filename}'
 
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    avatar = CloudinaryField('avatar')
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateTimeField(null=True, blank=True)
@@ -18,9 +16,10 @@ class Profile(models.Model):
 
     @property
     def avatar_url(self):
-        if self.avatar and hasattr(self.avatar, 'url'):
-            return self.avatar.url
+        if self.avatar and hasattr(self.avatar, 'public_id'):
+            return self.avatar.build_url(width=150, height=150, crop='thumb', gravity='face')
         return static('img/default_avatar.jpg')
+
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
